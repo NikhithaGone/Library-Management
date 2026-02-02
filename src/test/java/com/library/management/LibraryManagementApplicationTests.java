fae9ca431e84368986146b6258831fe1f1229cae
NikhithaGone <nikhithagone304@gmail.com>
2026-02-03 01:27:50 +0530

    Commented test case

--- src/test/java/com/library/management/LibraryManagementApplicationTests.java
+++ src/test/java/com/library/management/LibraryManagementApplicationTests.java
@@ -1,13 +1,12 @@
-package com.library.management;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LibraryManagementApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.library.management;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class LibraryManagementApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
